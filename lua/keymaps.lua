4ce2c742700718d668c2961f1ab0a9c5e83cd424
Jacob Tizel <jacob.tizel@gmail.com>
2023-12-10 23:48:30 -0800

    Initial customizations; undotree added
    Keymap added for :Ex, auto clipboard sync turned off.
    
    Undotree fix

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,6 +14,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Shortcut for Ex mode
+vim.keymap.set('n', '<leader>E', vim.cmd.Ex, { desc = 'Open [E]x file explorer' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
--- lua/options.lua
+++ lua/options.lua
@@ -8,13 +8,16 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Set relative line numbers
+vim.wo.relativenumber = true;
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+--vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
