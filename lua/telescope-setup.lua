4c17a88b29b86297ff1783ffc444f4173e773431
Jacob Tizel <jacob.tizel@gmail.com>
2023-12-14 23:11:01 -0800

    Lexima plugin added, telescope bindings added
    lexima: bracket completion plugin
    
    telescope: treesitter search and telescope func search added

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -77,5 +77,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>st', require('telescope.builtin').treesitter, { desc = '[S]earch [T]reesitter list' })
+vim.keymap.set('n', '<leader>s?', require('telescope.builtin').builtin, { desc = '[S]earch Telescope Functions' })
 
 -- vim: ts=2 sts=2 sw=2 et
