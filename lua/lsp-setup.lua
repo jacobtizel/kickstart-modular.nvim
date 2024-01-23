6ea222eeac88e02011b00153dc8e4af9c3b85c3f
Jacob Tizel <jacob.tizel@gmail.com>
2024-01-22 23:40:15 -0800

    HTML lsp added

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -80,7 +80,7 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
