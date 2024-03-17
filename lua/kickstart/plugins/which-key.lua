2ddb8acb90668e593bc43e61ac92845bcc4f0708
Jacob Tizel <jacob.tizel@gmail.com>
2024-03-17 15:18:46 -0700

    feat(which-key): more prefixes added

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -22,10 +22,16 @@ return {
 
       -- Document existing key chains
       require('which-key').register {
+        ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]arpoon/[H]unk', _ = 'which_key_ignore' },
+        ['<leader>m'] = { name = '[M]arkdown', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]indow/[W]orkspace', _ = 'which_key_ignore' },
         ['<leader><tab>'] = { name = '[Tab]', _ = 'which_key_ignore' },
       }
