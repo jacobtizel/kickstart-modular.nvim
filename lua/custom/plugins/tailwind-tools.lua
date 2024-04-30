97d2ed327b953f6bcec073c2251f833197e01658
Jacob Tizel <jacob.tizel@gmail.com>
2024-04-30 15:44:14 -0700

    fix(plugins): Inline tailwind colors not supported, change to bg
    Revert whenever this gets added to nvim stable (currently 0.9.5)

--- lua/custom/plugins/tailwind-tools.lua
+++ lua/custom/plugins/tailwind-tools.lua
@@ -2,5 +2,10 @@
 return {
   'luckasRanarison/tailwind-tools.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {}, -- your configuration
+  opts = {
+    document_color = {
+      -- defaults to "inline", currently set since not supported in nvim stable (0.9.5)
+      kind = 'background',
+    },
+  }, -- your configuration
 }
