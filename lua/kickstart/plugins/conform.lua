c89e7ca23de1d2f56b4e039daf0e65034dc573da
Jacob Tizel <jacob.tizel@gmail.com>
2025-02-06 09:42:53 -0800

    conform JS changes, autoformat enable/disable

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -26,6 +26,9 @@ return {
         else
           lsp_format_opt = 'fallback'
         end
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         return {
           timeout_ms = 500,
           lsp_format = lsp_format_opt,
@@ -38,8 +41,26 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'biome', stop_after_first = true, lsp_format = 'never' },
       },
     },
   },
+  vim.api.nvim_create_user_command('FormatDisable', function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = 'Disable autoformat-on-save',
+    bang = true,
+  }),
+  vim.api.nvim_create_user_command('FormatEnable', function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = 'Re-enable autoformat-on-save',
+  }),
 }
 -- vim: ts=2 sts=2 sw=2 et
