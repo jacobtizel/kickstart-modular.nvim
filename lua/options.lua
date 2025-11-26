cf83aad3578b930d041a04b1253bc55b044660d3
Jacob Tizel <jacob.tizel@gmail.com>
2025-11-25 18:58:51 -0800

    neovide 15.2 fixes

--- lua/options.lua
+++ lua/options.lua
@@ -96,7 +96,7 @@ vim.o.scrolloff = 10
 
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_opacity = 0.95
   vim.g.neovide_theme = 'auto'
   vim.g.neovide_cursor_vfx_mode = 'railgun'
 end
